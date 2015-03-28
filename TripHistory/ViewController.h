dda82d229d7dd824394a23e118e010a9b31ffc63
Gilad Gurantz <gilad@pb>
2015-03-28 14:02:06 -0700

    Change to Objective C

---
+++ TripHistory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TripHistory
+//
+//  Created by Gilad Gurantz on 3/28/15.
+//  Copyright (c) 2015 Lyft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
